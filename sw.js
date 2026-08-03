7eb89f40b424634a1566eb3065028b24a9e6c7e5
jvsouza01 <jao_vithor@hotmail.com>
2026-08-03 14:51:54 -0300

    Update SW cache to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'estudoq-v1';
+const CACHE_NAME = 'estudoq-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -10,8 +10,23 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
